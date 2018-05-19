27491e5c54ccf0734f4f7d6c1472add5a619a04c
Christian Larzep <christian.dm2@hotmail.com>
2018-05-18 20:02:46 -0500

    Ruby bot with steps

---
+++ Gemfile
@@ -0,0 +1,7 @@
+source 'http://rubygems.org'
+
+gem 'http' ##http protocol
+gem 'json' ##slack has an json api that can allow the comunication whit it
+gem 'faye-websocket' ##Mantain a permanent connection
+gem 'eventmachine'
+gem 'sinatra'
---
+++ play.rb
@@ -0,0 +1,103 @@
+require 'http'
+require 'json'
+require 'eventmachine'
+require 'faye/websocket'
+require 'sinatra'
+
+Thread.new do
+  EM.run do
+  end
+end
+
+get '/' do
+  if params['code']
+    rc = HTTP.post("https://slack.com/api/oauth.access", params:{
+      client_id: ENV['SLACK_CLIENT_ID'],
+      client_secret: ENV['SLACK_CLIENT_SECRET'],
+      code: params['code']
+    })
+    rc = JSON.parse(rc)
+    token = rc['bot']['bot_access_token']
+
+    rc = HTTP.post("https://slack.com/api/rtm.start", params:{
+      token: token
+    })
+    rc = JSON.parse(rc.body)
+    url = rc['url']
+
+    ws = Faye::WebSocket::Client.new(url)
+    ws.on :open do
+      p [:open]
+    end
+
+    ws.on :message do |event|
+      p [:message, JSON.parse(event.data)]
+      data = JSON.parse(event.data)
+      if data['text'] == 'hi'
+        ws.send({ type: 'message',
+          text: "HI, ya se pudo <@#{data['user']}>",
+          channel: data['channel'] }.to_json)
+      end
+    end
+
+    ws.on :close do
+      p [:close, event.code, event.reason]
+      ws = nil
+      EM.stop
+    end
+    "Bot succesfully installed"
+  else
+    "Hello world"
+  end
+end
+
+=begin
+client_id
+358191024146.366203559638
+
+client_secret
+9fe16fc7b2b733f487e982fecafec4c2
+
+pm1WTTzg9p7LJSscjo5vDwmX
+
+OAuth Access Token
+xoxp-358191024146-358197559218-365312370274-18a2397f5d764e3eb58c0306744c6e27
+
+Bot User OAuth Access Token
+xoxb-358191024146-365312372466-s8k2D9sthjgffR7enQYt6TfW
+
+https://api.slack.com/apps/AAS5ZGFJS/distribute?
+EM.run do
+  ws = Faye::websocket::Client.new(url)
+  ws.on :open do
+    p [:open]
+  end
+
+  ws.on :message do |event|
+    p [:message, JSON.parse(event.data)]
+    data = JSON.parse(event.data)
+    if data['text'] == 'hi'
+      ws.send({ type: 'message',
+        text: "hi <@#{data['user']}>",
+        channel: data['channel'] }.to_json)
+    end
+  end
+
+  ws.on :close do
+    p [:close, event.code, event.]
+    ws = nil
+    EM.stop
+  end
+end
+##bundle exec ruby play.rb
+##export SLACK_API_TOKEN=xoxb-358347473157-V93EaFSRQ7ZirOc49zUzyII0
+
+##Send messages
+##rc = HTTP.post("https://slack.com/api/chat.postMessage", params:{
+  ##token: ENV['SLACK_API_TOKEN'],
+  ##channel: '#general',
+  ##text: '',
+  ##as_user: true
+  ##})
+##puts JSON.pretty_generate(JSON.parse(rc.body))
+=end
